8b60ed941e9cf79c324ee141e1f5f2d5c227cb8c
joejoh <georgeja.18@student.funaab.edu.ng>
2025-06-21 17:33:48 +0100

    2nd

--- App.js
+++ App.js
@@ -1,3 +1,40 @@
-var logger = require("./logger");
+// const logger = require("./logger");
 
-logger.log('message')
+// logger = 1;
+
+// logger.log('message ')
+
+const path = require('path')
+
+var pathObj = path.parse(__filename);
+
+console.log(pathObj);
+
+const os = require('os');
+
+var totalMemory = os.totalmem();
+var freeMemory = os.freemem();
+
+// console.log('Total memory:'+ totalMemory);
+
+//template string 
+// ES6 / Es2015 : ECMAScript 6
+
+console.log(`Total Memory:${totalMemory}`);
+console.log(`Free Memory: ${freeMemory}`);
+
+
+const fs = require('fs')
+const files = fs.readdirSync('./')
+console.log(files);
+
+
+fs.readdir('./', function(err, files){
+    if(err) console.log('error', err);
+    else console.log('result', files);
+    
+    
+});
+
+
+
