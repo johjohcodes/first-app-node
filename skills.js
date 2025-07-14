6c3047789f671bf51fe64be1838ee77748eef47e
joejoh <georgeja.18@student.funaab.edu.ng>
2025-07-14 23:14:51 +0100

    mehnn

---
+++ skills.js
@@ -0,0 +1,30 @@
+
+
+
+function runner(){
+  let strings = [1,2,3,4,5,6,2,4,4]
+  for (let i = 0; i < strings.length; i++) {
+    console.log(strings[i]);
+        
+    
+}
+}
+
+runner();
+
+
+const users = () => {
+    
+}
+maxArry = [45,3,5,4,2]
+
+for (let i = 0; i < maxArry.length; i++) {
+  const newArry = maxArry[i];
+  newArry.sort()
+  
+  if (condition) {
+    
+  }
+  
+}
+
