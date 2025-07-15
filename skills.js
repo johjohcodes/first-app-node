5fda390b40887d22cde2dc420040afe3b2eb2fc5
joejoh <georgeja.18@student.funaab.edu.ng>
2025-07-15 07:57:57 +0100

    uu

--- skills.js
+++ skills.js
@@ -1,30 +1,31 @@
 
 
 
-function runner(){
-  let strings = [1,2,3,4,5,6,2,4,4]
-  for (let i = 0; i < strings.length; i++) {
-    console.log(strings[i]);
+// function runner(){
+//   let strings = [1,2,3,4,5,6,2,4,4]
+//   for (let i = 0; i < strings.length; i++) {
+//     console.log(strings[i]);
         
     
-}
-}
+// }
+// }
 
-runner();
+// runner();
 
 
 const users = () => {
     
 }
 maxArry = [45,3,5,4,2]
+max = [0];
 
 for (let i = 0; i < maxArry.length; i++) {
-  const newArry = maxArry[i];
-  newArry.sort()
-  
-  if (condition) {
+ maxArry[i];
+   if (maxArry[i] > max[0]) {
+    max[0] = maxArry[i];
+   }
     
   }
   
-}
+  console.log(max);
 
