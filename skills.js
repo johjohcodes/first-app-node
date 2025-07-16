204feaf9440da131c82fe51cdf71ec68432435e3
joejoh <georgeja.18@student.funaab.edu.ng>
2025-07-16 18:07:57 +0100

    okay

--- skills.js
+++ skills.js
@@ -30,41 +30,47 @@ for (let i = 0; i < maxArry.length; i++) {
   console.log(max);
 
 
-  const ROCK = 'rock'
-  const SCISSORS = 'scissors'
-  const PAPER = 'paper'
+  const ROCK = document.getElementById('rock')
+  const SCISSORS = document.getElementById('scissors')
+  const PAPER = document.getElementById('paper')
+  const resultDiv = document.getElementById('result');
 
-  function playerMove(ROCK, SCISSORS, PAPER){
-      if (condition) {
-        
-      }
+  function playGame(playerMove){
+    const computer = computerMove()
+    const result = getWinner(playerMove, computer)
+    resultDiv.textContent = `Player: ${playerMove} | Computer: ${computer} | ${result}`;
+  }
+
+  function getWinner(player, computer){
+    if(player === computer) return 'it a tie';
+    if (
+      (player === 'rock' && computer === "scissors") ||
+      (player === 'paper' && computer === 'rock') ||
+      (player === 'scissors' && computer === 'paper')
+    ){
+      return 'Player wins'
+    }
+    return ' computer wins'
+
   }
 
   function computerMove(){
-      let random = Math.random()
-      if (random > 1/3) {
-         console.log('paper');
-
-         
-      } else if (random > 2/3){
-        console.log('rock');
-        
-    
-      }  else {
-        console.log('scissors');
-        
-      }
-      console.log(random);
+      const moves = ['rock','paper', 'scissors' ]
+      const random = Math.floor(Math.random()* moves.length)
+      return moves[random]
+      
       
   }
-  computerMove()
+ 
 
 
 
   // there will be a startgame function
   // playerMove function
   // computerMove function
+  // create a button link it to player move 
 
 
-
-
+ROCK.onclick = () => playGame('rock');
+PAPER.onclick = () => playGame('paper');
+SCISSORS.onclick = () => playGame('scissors');
